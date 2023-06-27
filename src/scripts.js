4c030fa3e688993371a44c2eb3c9b2caa77f0bae
Dennis Herrera <dennishumher25@hotmail.com>
2023-06-26 23:00:28 -0600

    added accordion transitions scripts

---
+++ src/scripts.js
@@ -0,0 +1,38 @@
+"use-strict";
+
+function headerClick (headerTextId, itemBodyId, itemHeaderArrow) {
+  const headersText = document.querySelectorAll(".header-text");
+  const clickedHeaderText = headerTextId;
+  const accordionBodies = document.querySelectorAll(".item-body");
+  const clickedItemBody = itemBodyId;
+  const accordionArrows = document.querySelectorAll(".arrow")
+  const clickedItemArrow = itemHeaderArrow;
+
+  headersText.forEach((header) => {
+    if (header != headerTextId) {
+      header.classList.remove("bold");
+    }
+  })
+
+  accordionBodies.forEach((body) => {
+    if (body != itemBodyId) {
+      body.classList.add("hidden");
+    }
+  })
+
+  accordionArrows.forEach((arrow) => {
+    if (arrow != itemHeaderArrow) {
+      arrow.classList.remove("rotate");
+    }
+  })
+
+  if (clickedItemBody.classList.contains("hidden")) {
+    clickedHeaderText.classList.add("bold");
+    clickedItemBody.classList.remove("hidden");
+    clickedItemArrow.classList.add("rotate");
+  } else {
+    clickedHeaderText.classList.remove("bold");
+    clickedItemBody.classList.add("hidden"); 
+    clickedItemArrow.classList.remove("rotate");
+  }
+}
